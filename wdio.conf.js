28fc26f58b6befebbf9c3572f814ff8a85c5ba59
Max <37596205+mdsauce@users.noreply.github.com>
2018-03-28 15:20:36 -0700

    Add appium-asserts-waits (#1)
    * Added another set of tests.
    
    * Removed the parrallel test package as wdio already runs each capability in parallel
    
    * Removed any unused packages.  Added standard test waits.  Added asserts.

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,36 +1,48 @@
+let mochaTimeout = 100000;
+
 var config = {
-    
     protocol: 'https',
     host: 'us1.appium.testobject.com',
     // host: 'eu1.appium.testobject.com'
     port: '443',
     path: '/wd/hub',
-    
 
-    // baseUrl: 'https://us1.appium.testobject.com/wd/hub:443',
-
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
+    // For use with wdio-sauce-service https://github.com/webdriverio/wdio-sauce-service
+    // user: process.env.SAUCE_USERNAME,
+    // key: process.env.SAUCE_ACCESS_KEY,
     // sauceConnect: true, // Optional use of Sauce Connect Proxy http://webdriver.io/guide/services/sauce.html#sauceConnect 
 
+    /* Optional maxInstances value limits the number of child processes spawned.
+    * Each capability defined gets a child process so that tests automatically run in parallel
+    * Refer to for more info https://github.com/webdriverio/webdriverio/issues/205#issuecomment-183345085
+    */
     maxInstances: 20,
-    capabilities: [{
-        testobject_api_key: 'D377483757DE4C7FA46B0DBDEF9540AA',
-        platformName: 'Android',
-        browserName: 'chrome',
-        /* supply `platformVersion` without `deviceName` to get any available device with that version dynamically */
-        // platformVersion: '7', // Optional
-        // deviceName: 'Samsung Galaxy S8' // Optional
-        testobject_test_name: 'Basic Nodejs Test',
-        // tunnelIdentifier: 'fakeBusiness' // Optional tunnel
-    },
-    {
-        testobject_api_key: 'D377483757DE4C7FA46B0DBDEF9540AA',
-        platformName: 'iOS',
-        browserName: 'chrome',
-        testobject_test_name: 'Basic Nodejs Test',
-        // tunnelIdentifier: 'fakeBusiness' // Optional tunnel
-    }
+    capabilities: [
+        {
+            testobject_api_key: 'D377483757DE4C7FA46B0DBDEF9540AA',
+            platformName: 'Android',
+            browserName: 'chrome',
+            /* supply `platformVersion` without `deviceName` to get any available device with that version dynamically */
+            // platformVersion: '7', // Optional
+            // deviceName: 'Samsung Galaxy S8' // Optional
+            testobject_test_name: 'Basic Nodejs Test',
+            // tunnelIdentifier: 'fakeBusiness' // Optional tunnel
+        },
+        {
+            testobject_api_key: 'D377483757DE4C7FA46B0DBDEF9540AA',
+            platformName: 'iOS',
+            browserName: 'chrome',
+            testobject_test_name: 'Basic Nodejs Test',
+            // tunnelIdentifier: 'fakeBusiness' // Optional tunnel
+        },
+        {
+            testobject_api_key: 'D377483757DE4C7FA46B0DBDEF9540AA',
+            platformName: 'iOS',
+            browserName: 'safari',
+            browserVersion: 'latest',
+            testobject_test_name: 'Basic Nodejs Test',
+            // tunnelIdentifier: 'fakeBusiness' // Optional tunnel
+        }
     ],
 
     specs: [
@@ -41,13 +53,14 @@ var config = {
     logLevel: 'verbose',
     coloredLogs: true,
     screenshotPath: './errorShots/',
-    reporters: ['spec'],
+    reporters: ['spec'], // Optional with wdio-spec-reporter
     // waitforTimeout: 10000,
     connectionRetryTimeout: 10 * 60000,
     connectionRetryCount: 3,
     framework: 'mocha',
     mochaOpts: {
-        ui: 'bdd'
+        ui: 'bdd',
+        timeout: mochaTimeout
     }
 }
 
